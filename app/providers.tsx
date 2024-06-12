2a0ebee3cb0e944fbfa3a2e64627983ed569606e
zedjarvis <omondicedo@gmail.com>
2024-06-13 02:01:36 +0300

    feat: add chakra ui provider

---
+++ app/providers.tsx
@@ -0,0 +1,9 @@
+// app/providers.tsx
+"use client";
+
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <ChakraProvider>{children}</ChakraProvider>;
+}
